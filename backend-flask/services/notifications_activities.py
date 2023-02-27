512646dcd1b565a4898c658117c2c3920a037875
Hiba <khibafcm@gmail.com>
2023-02-27 21:51:48 +0000

    some bug fixes on notifications UI

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -5,7 +5,7 @@ class NotificationsActivities:
      results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Hiba',
-      'message': 'I`m a unicorn!',
+      'message': 'Hiba\'s backpain is moderate to severe',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -15,7 +15,7 @@ class NotificationsActivities:
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Worf',
-        'message': 'This post has no honor!',
+        'message': 'lol that\'s funny!',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
